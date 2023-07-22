d8f5f24b50597bf7525bae0995e172b512c4cf7c
SeungGukYoo <dbtmd878@gmail.com>
2023-07-22 12:35:17 +0900

    react-hook-form을 이용하여 로그인 컴포넌트 생성

---
+++ components/login.tsx
@@ -0,0 +1,57 @@
+"use client";
+import UserContext from "@/context/userInfo";
+import React, { useContext, useEffect } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+type Inputs = {
+  id: string;
+  password: string;
+  isLoggedIn: boolean;
+};
+
+function Login() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const context = useContext(UserContext);
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    if (context && !context.state.user.isLoggedIn) {
+      console.log(context?.state.user.isLoggedIn);
+      context.dispatch({
+        type: "LOGIN",
+        payload: {
+          name: "SeungGuk",
+          email: data.id,
+          isLoggedIn: true,
+        },
+      });
+    }
+  };
+
+  return (
+    <div className="">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+        {/* <form className="flex flex-col" onSubmit={subm}> */}
+        <label>ID:</label>
+        <input type="text" className=" text-black block" {...register("id", { required: "This is required ID" })} />
+
+        <label>PASSWORD:</label>
+        <input
+          type="password"
+          className=" text-black block"
+          {...register("password", { required: "This is required PASSWORD" })}
+        />
+
+        <div className="flex justify-around">
+          <input type="submit" value="LOGIN" />
+          <button>CLOSE</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Login;

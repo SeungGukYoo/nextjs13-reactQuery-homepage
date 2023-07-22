b48d84668ce2dcae70e1eeafe28dd4222de6233d
SeungGukYoo <dbtmd878@gmail.com>
2023-07-22 12:31:16 +0900

    react-query provider 설정

---
+++ app/providers.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React, { useState } from "react";
+type Props = {
+  children: React.ReactNode;
+};
+
+function Providers({ children }: Props) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            retry: false,
+          },
+        },
+      })
+  );
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default Providers;

19667112113fd02adaac53268f7039504ee29e58
SeungGukYoo <dbtmd878@gmail.com>
2023-07-22 12:32:25 +0900

    글로벌 컴포넌트 Nav,Footer추가

---
+++ components/globalNav.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function GlobalNav() {
+  return (
+    <div className="max-w-7xl h-12 w-full">
+      <ul className="flex h-full items-center justify-around">
+        <li>HOME</li>
+        <li>LIST</li>
+        <li>MY PAGE</li>
+        <li>LOGIN</li>
+      </ul>
+    </div>
+  );
+}
+
+export default GlobalNav;
